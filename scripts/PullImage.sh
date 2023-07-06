2a88c5f1044fa469595ff477d7152cf15761e04c
akashjavali <akash.javali@gmail.com>
2023-07-06 23:28:35 +0530

    cleared some unwanted files n packages

--- scripts/PullImage.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -e
-IMAGE_NAME=556200905665.dkr.ecr.ap-south-1.amazonaws.com/wybrid-vendor-frontend-production
-
-#pull latest image from ecr
-docker pull $IMAGE_NAME
--- scripts/Update.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -e
-STACK_FILE=/home/ubuntu/docker/vendor_frontend/docker-compose.yml
-
-#update docker container with new image
-docker compose -f $STACK_FILE up -d --build
